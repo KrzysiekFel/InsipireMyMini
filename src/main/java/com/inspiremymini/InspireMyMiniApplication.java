18dbdecc64cb254527da2c0407c26dfaa96ac1d6
Krzysiek <krzysztof.feliksiak@gmail.com>
2026-02-15 12:43:53 +0100

    Initial commit

---
+++ src/main/java/com/inspiremymini/InspireMyMiniApplication.java
@@ -0,0 +1,13 @@
+package com.inspiremymini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspireMyMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspireMyMiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inspiremymini/InspireMyMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspiremymini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspireMyMiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
